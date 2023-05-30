b24154f71211c6d8b49b3089a46a8643bad0f88a
Lucas Farias <lucas.fariasag@gmail.com>
2023-05-29 22:07:35 -0300

    first commit

---
+++ src/test/java/com/api/bompreparo/BomPreparoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.bompreparo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BomPreparoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
